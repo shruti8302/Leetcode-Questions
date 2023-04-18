5fc3b756b22e2814ea2c57a8948438f49df18d4c
shruti8302 <93856491+shruti8302@users.noreply.github.com>
2023-04-18 23:54:38 +0530

    Added solution - LeetHub

---
+++ Greater on right side - GFG/greater-on-right-side.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	/* Function to replace every element with the
+	next greatest element */
+	void nextGreatest(int arr[], int n) {
+	    int maxelement = -1;
+	    for(int i = n-1;i >= 0;i--){
+	        int temp = maxelement;
+	        if(arr[i] > maxelement){
+	            maxelement = arr[i];
+	        }
+	        arr[i] = temp;
+	    }
+	}
+
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, l, r;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.nextGreatest(arr, n);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
