c1d1f8479dd20ba81de15a9b9163cb3dd86729ec
Shruti Singh <93856491+shruti8302@users.noreply.github.com>
2023-05-18 20:51:25 +0530

    Time: 66 ms (50.23%), Space: 14.5 MB (88.26%) - LeetHub

---
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    
+    struct node{
+        int data, row, col;
+        node(int val, int i, int j): data(val), row(i), col(j) {}
+    };
+    
+    struct comp{
+        bool operator()(node a, node b){
+            return a.data > b.data;
+        }
+    };
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        int k = nums.size();
+        priority_queue<node, vector<node>, comp> minHeap;
+        int currMin = INT_MAX, currMax = INT_MIN, currRange = INT_MAX;
+        
+        for(int i = 0;i < k;i++){
+            currMax = max(currMax,nums[i][0]);
+            node temp(nums[i][0],i,0);
+            minHeap.push(temp);
+        }
+        
+        int start = currMin, end = currMax;
+        
+        while(true){
+            node min = minHeap.top();
+            minHeap.pop();
+            currMin = min.data;
+            if(currMax-currMin < currRange){
+                start = currMin;
+                end = currMax;
+                currRange = end - start;
+            }
+            if(min.col + 1 == nums[min.row].size()){
+                break;
+            }
+            node next(nums[min.row][min.col+1], min.row, min.col+1);
+            minHeap.push(next);
+            
+            if(next.data > currMax){
+                currMax = next.data;
+            }
+        }
+        return {start, end};
+    }
+};
